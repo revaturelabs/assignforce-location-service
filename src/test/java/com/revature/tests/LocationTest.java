4a574d8fb2359b66f37530f587ab8b8a325048b3
Rafael <rlousta13@gmail.com>
2018-08-01 15:05:35 -0400

    First commit

--- src/test/java/com/revature/tests/LocationTest.java
+++ src/test/java/com/revature/tests/LocationTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.revature.assignforce.beans.Building;
 import com.revature.assignforce.beans.Location;
 import com.revature.assignforce.beans.Room;
+///washere
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
