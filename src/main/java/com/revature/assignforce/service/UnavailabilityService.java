6f63697c3063b5ab5dd449fa54ca89441e6ea3c1
Shannon Massey <shannonbmassey22@gmail.com>
2018-08-01 16:39:37 -0400

    started unavailability service

---
+++ src/main/java/com/revature/assignforce/service/UnavailabilityService.java
@@ -0,0 +1,34 @@
+package com.revature.assignforce.service;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UnavailabilityService {
+	
+	/* Will uncomment when a repository is created and Unavailability bean is moved to this project
+	 * 
+	@Autowired
+	private UnavailabilityRepo unavailRepo;
+	
+	public List<Unavailability> getAll() {
+		return unavailRepo.findAll();
+	}
+
+	public Optional<Unavailability> findById(int id) {
+		return unavailRepo.findById(id);
+	}
+
+	public Unavailability update(Unavailability u) {
+		return unavailRepo.save(u);
+	}
+
+	public Unavailability create(Unavailability u) {
+		return unavailRepo.save(u);
+	}
+
+	public void delete(int id) {
+		unavailRepo.deleteById(id);
+	}
+	 */
+}
