2b71cd372c27f4e8855e39414f8538d9a7a01f65
kthile <khang_thile@yahoo.com>
2019-05-01 15:06:41 -0400

    added mockito tests for eventservice + impl

--- src/test/java/com/revature/tests/event/EventServiceImplTest.java
+++ src/test/java/com/revature/tests/event/EventServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.revature.tests.event;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -56,7 +58,54 @@ public class EventServiceImplTest {
 		Mockito.when(eventRepository.findAll()).thenReturn(eventList);
 		List<Event> testList = eventService.getAll();
 		assertEquals(2, testList.size());
-
+	}
+	
+	@Test
+	public void findByIdTest() {
+		LocalDate s1 = LocalDate.of(1994, 4, 27);
+		LocalDate e1 = LocalDate.of(1994, 4, 27);
+		LocalDate t1 = LocalDate.now();
+		int id = 1;
+		Event e = new Event(id, s1, e1, "Name", t1, 2);
+		Optional<Event> op1 = Optional.ofNullable(e);
+		Mockito.when(eventRepository.findById(id)).thenReturn(op1);
+		Optional<Event> test = eventService.findById(id);
+		assertEquals("Name", test.get().getName());
+	}
+	
+	@Test
+	public void updateTest() {
+		LocalDate s1 = LocalDate.of(1994, 4, 27);
+		LocalDate s2 = LocalDate.of(1994, 5, 27);
+		LocalDate s3 = LocalDate.now();
+		
+		Event e = new Event(1, s1, s2, "Name", s3, 2);
+		e.setEndDate(LocalDate.of(1994, 5, 20));
+		Mockito.when(eventRepository.save(e)).thenReturn(e);
+		Event test = eventService.update(e);
+		assertEquals(LocalDate.of(1994, 5, 20), test.getEndDate());	
+	}
+	
+	@Test
+	public void createTest() {
+		LocalDate s1 = LocalDate.of(1994, 4, 27);
+		LocalDate s2 = LocalDate.of(1994, 5, 27);
+		LocalDate s3 = LocalDate.now();
+		
+		int id = 10;
+		Event e = new Event(1, s1, s2, "Name", s3, 2);
+		Mockito.when(eventRepository.save(e)).thenReturn(e);
+		Event test = eventService.create(e);
+		assertEquals(id, test.getId());
+	}
+	
+	@Test
+	public void deleteTest() {
+		int id = 22;
+		Mockito.doNothing().when(eventRepository).deleteById(id);
+		eventService.delete(id);
+		Optional<Event> test = eventService.findById(id);
+		assertFalse(test.isPresent());
 	}
 
 }
