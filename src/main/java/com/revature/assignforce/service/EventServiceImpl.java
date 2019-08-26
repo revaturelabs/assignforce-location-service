e7d7b7e5b341facd0298b85773fd0190060f538b
August Duet <august.duet@revature.com>
2019-08-26 12:04:38 -0400

    add service annotation to fix bean wiring bug

--- src/main/java/com/revature/assignforce/service/EventServiceImpl.java
+++ src/main/java/com/revature/assignforce/service/EventServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.assignforce.beans.Event;
 import com.revature.assignforce.repos.EventRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EventServiceImpl implements EventService {
 
 	@Autowired
